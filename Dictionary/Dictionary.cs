1b2fd44a2c00a17aa506601ee41e869779513518
BaladaRock <48443239+BaladaRock@users.noreply.github.com>
2019-06-12 17:04:05 +0300

    Dictionary<TKey, TValue> - fully implemented

--- Dictionary/Dictionary.cs
+++ Dictionary/Dictionary.cs
@@ -49,25 +49,38 @@ namespace Dictionary
 
         public int Count { get; private set; }
 
-        public bool IsReadOnly { get; }
+        public bool IsReadOnly { get; private set; }
 
         public TValue this[TKey key]
         {
             get
             {
-                throw new NotImplementedException();
+                ThrowArgumentIsNull(key);
+                if (FindElement(key) == -1)
+                {
+                    throw new KeyNotFoundException($"Key {key} was not found! ");
+                }
+
+                return elements[FindElement(key)].Value;
             }
 
             set
             {
-                throw new NotImplementedException();
+                ThrowReadOnly();
+                ThrowArgumentIsNull(key);
+                if (FindElement(key) == -1)
+                {
+                    throw new KeyNotFoundException($"Key {key} was not found! ");
+                }
+
+                elements[FindElement(key)].Value = value;
             }
         }
 
         public void Add(TKey key, TValue value)
         {
             ThrowAddExceptions(key);
-            AddElement(ref elements[Count], key, value);
+            elements[Count].Update(key, value);
 
             int bucketIndex = GetFirstBucketPosition(key);
             if (buckets[bucketIndex] != -1)
@@ -86,38 +99,40 @@ namespace Dictionary
 
         public void Clear()
         {
+            ThrowReadOnly();
+            Array.Fill(buckets, -1);
             Count = 0;
         }
 
         public bool Contains(KeyValuePair<TKey, TValue> item)
         {
-            throw new NotImplementedException();
+            return FindElement(item.Key, item.Value) != -1;
         }
 
         public bool ContainsKey(TKey key)
         {
             ThrowArgumentIsNull(key);
-            for (int temp = buckets[GetFirstBucketPosition(key)]; temp != -1; temp = elements[temp].Next)
-            {
-                if (elements[temp].Key.Equals(key))
-                {
-                    return true;
-                }
-            }
 
-            return false;
+            return FindElement(key) != -1;
         }
 
         public void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex)
         {
-            throw new NotImplementedException();
+            ThrowCopyToExceptions(array, arrayIndex);
+
+            var enumerator = GetEnumerator();
+            for (int i = arrayIndex; i < Count + arrayIndex; i++)
+            {
+                enumerator.MoveNext();
+                array[i] = enumerator.Current;
+            }
         }
 
         public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
         {
             for (int i = 0; i < Count; i++)
             {
-                yield return GetBuckets(elements[i]);
+                yield return CreateKeyValuePair(elements[i]);
             }
         }
 
@@ -128,17 +143,63 @@ namespace Dictionary
 
         public bool Remove(TKey key)
         {
-            throw new NotImplementedException();
+            ThrowArgumentIsNull(key);
+            ThrowReadOnly();
+
+            int index = FindElement(key, out int previous);
+            if (index == -1)
+            {
+                return false;
+            }
+
+            if (previous != -1)
+            {
+                elements[previous].Next = elements[index].Next;
+            }
+            else
+            {
+                buckets[GetFirstBucketPosition(key)] = elements[index].Next;
+            }
+
+            Count--;
+            return true;
         }
 
         public bool Remove(KeyValuePair<TKey, TValue> item)
         {
-            throw new NotImplementedException();
+            if (FindElement(item.Key, item.Value) != FindElement(item.Key))
+            {
+                return false;
+            }
+
+            return Remove(item.Key);
         }
 
         public bool TryGetValue(TKey key, out TValue value)
         {
-            throw new NotImplementedException();
+            ThrowArgumentIsNull(key);
+
+            if (FindElement(key) == -1)
+            {
+                value = default;
+                return false;
+            }
+
+            value = elements[FindElement(key)].Value;
+            return true;
+        }
+
+        public HashtableDictionary<TKey, TValue> AsReadOnly()
+        {
+            var newDictionary = new HashtableDictionary<TKey, TValue>();
+            var enumerator = GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                newDictionary.Add(enumerator.Current);
+            }
+
+            newDictionary.IsReadOnly = true;
+            return newDictionary;
         }
 
         private int GetFirstBucketPosition(TKey key)
@@ -146,18 +207,11 @@ namespace Dictionary
             return Math.Abs(key.GetHashCode()) % buckets.Length;
         }
 
-        private void AddElement(ref Element element, TKey key, TValue value)
-        {
-            element.Key = key;
-            element.Value = value;
-            element.Next = -1;
-        }
-
         private void ThrowAddExceptions(TKey key)
         {
+            ThrowReadOnly();
             ThrowArgumentIsNull(key);
             ThrowArgument(key);
-            ThrowReadOnly();
         }
 
         private void ThrowReadOnly()
@@ -167,7 +221,7 @@ namespace Dictionary
                 return;
             }
 
-            throw new NotSupportedException($"List {this} is readonly! ");
+            throw new NotSupportedException("List is readonly!\n");
         }
 
         private void ThrowArgument(TKey key)
@@ -187,10 +241,47 @@ namespace Dictionary
                 return;
             }
 
-            throw new ArgumentNullException(paramName: $"{key}");
+            throw new ArgumentNullException(paramName: nameof(key));
         }
 
-        private KeyValuePair<TKey, TValue> GetBuckets(Element element)
+        private void ThrowCopyToExceptions(KeyValuePair<TKey, TValue>[] array, int arrayIndex)
+        {
+            ThrowArrayIsNull(array);
+            ThrowIndexException(arrayIndex);
+            ThrowArgumentException(array, arrayIndex);
+        }
+
+        private void ThrowArgumentException(KeyValuePair<TKey, TValue>[] array, int arrayIndex)
+        {
+            if (array.Length >= Count + arrayIndex)
+            {
+                return;
+            }
+
+            throw new ArgumentException(message: "Copying proccess cannot be initialised\n", paramName: nameof(array));
+        }
+
+        private void ThrowIndexException(int arrayIndex)
+        {
+            if (arrayIndex >= 0)
+            {
+                return;
+            }
+
+            throw new ArgumentOutOfRangeException(paramName: nameof(arrayIndex), message: "Give a valid index!\n");
+        }
+
+        private void ThrowArrayIsNull(KeyValuePair<TKey, TValue>[] array)
+        {
+            if (array != null)
+            {
+                return;
+            }
+
+            throw new ArgumentNullException(paramName: nameof(array));
+        }
+
+        private KeyValuePair<TKey, TValue> CreateKeyValuePair(Element element)
         {
             return KeyValuePair.Create(element.Key, element.Value);
         }
@@ -200,6 +291,36 @@ namespace Dictionary
             return element.Key.Equals(key);
         }
 
+        private int FindElement(TKey key)
+        {
+            return FindElement(key, out int previous);
+        }
+
+        private int FindElement(TKey key, out int previousIndex)
+        {
+            previousIndex = -1;
+            for (int i = buckets[GetFirstBucketPosition(key)]; i != -1; i = elements[i].Next)
+            {
+                if (elements[i].Key.Equals(key))
+                {
+                    return i;
+                }
+
+                previousIndex = i;
+            }
+
+            return -1;
+        }
+
+        private int FindElement(TKey key, TValue value)
+        {
+            int index = FindElement(key);
+
+            return index != -1 && elements[index].Value.Equals(value)
+                ? index
+                : -1;
+        }
+
         private struct Element
         {
             public TKey Key { get; set; }
@@ -207,6 +328,13 @@ namespace Dictionary
             public TValue Value { get; set; }
 
             public int Next { get; set; }
+
+            public void Update(TKey key, TValue value, int next = -1)
+            {
+                Key = key;
+                Value = value;
+                Next = next;
+            }
         }
     }
 }
